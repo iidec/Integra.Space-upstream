b74a656e8d846555648ba9a3d92a7c1730361df4
Mariano <mgenovese@iid.ec>
2017-02-09 18:15:22 -0500

    Prints usando Trace

--- Integra.Space.StreamProviderTests/SpaceQueueAdapterUnitTests.cs
+++ Integra.Space.StreamProviderTests/SpaceQueueAdapterUnitTests.cs
@@ -31,8 +31,8 @@ namespace Integra.Space.StreamProviderTests
 			streamQueueMapper = new HashRingBasedStreamQueueMapper(1, "NN");
 			connectionString = new RedisConnectionString(TestContext.Properties["redisconnectionstring"].ToString());
 			invalidConnectionString = new RedisConnectionString(TestContext.Properties["invalidredisconnectionstring"].ToString());
-			System.Diagnostics.Debug.WriteLine("redisconnectionstring = '{0}'", TestContext.Properties["redisconnectionstring"].ToString());
-			System.Diagnostics.Debug.WriteLine("invalidredisconnectionstring = '{0}'", TestContext.Properties["invalidredisconnectionstring"].ToString());
+			System.Diagnostics.Trace.WriteLine(string.Format("redisconnectionstring = '{0}'", TestContext.Properties["redisconnectionstring"].ToString()));
+			System.Diagnostics.Trace.WriteLine(string.Format("invalidredisconnectionstring = '{0}'", TestContext.Properties["invalidredisconnectionstring"].ToString()));
 		}
 
 		public TestContext TestContext
